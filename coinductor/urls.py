a5436688376efacf566dd7776d8ea4cf74e16a02
UncleWoo <93mateuszkocur@gmail.com>
2026-06-18 10:10:38 +0200

    Add home page view

--- coinductor/urls.py
+++ coinductor/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Coinductor is running! 🚀")
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
 ]
